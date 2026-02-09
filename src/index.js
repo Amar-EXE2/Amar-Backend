7c17c3581dfc526ca68b4d61212161dc617064ba
Amar Satpute <amarsatpute32@gmail.com>
2026-02-09 17:16:40 +0530

    utils code

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv'
+import connectDB from "./db/database.js";
+import { app } from './app.js';
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB()
+
+.then(()=>{
+
+app.listen(process.env.PORT||8000,()=>{
+    console.log(`server is running on port${process.env.PORT}`);
+    
+})
+
+})
+
+.catch((eror)=> {
+
+    console.log(` database connnection faiild ${eror}`)
+    
+
+})
