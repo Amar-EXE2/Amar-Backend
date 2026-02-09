90ae857128cb22f5c363c1a260394cd1e6c72a7f
Amar Satpute <amarsatpute32@gmail.com>
2026-02-09 17:17:28 +0530

    utils code

--- src/app.js
+++ src/app.js
@@ -8,11 +8,16 @@ const app = express()
 
 app.use(cors({
 
-    origin:process.env.ORIGIN
+    origin:process.env.ORIGIN,
+    Credential:true
+
 }))
 
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true, limit : " 16kb "}))
+app.use(express.static("public"))
 
-
+app.use(cookieParser())
 export { app }
 
 
